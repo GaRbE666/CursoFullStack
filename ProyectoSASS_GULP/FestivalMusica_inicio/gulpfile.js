3df0a4358352bec3a098b68db1f9680cc5857007
GaRbE666 <jaimegon17@gmail.com>
2023-08-07 22:39:31 +0200

    Añadido mejoras JavaScript

--- ProyectoSASS_GULP/FestivalMusica_inicio/gulpfile.js
+++ ProyectoSASS_GULP/FestivalMusica_inicio/gulpfile.js
@@ -13,6 +13,9 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser-js');
+
 function css(done){
     src('src/scss/**/*.scss')//Identificar el archivo de SASS
     .pipe(sourcemaps.init()) 
@@ -64,6 +67,9 @@ function versionAvif(done){
 
 function javascript(done){
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'))
 
     done();
