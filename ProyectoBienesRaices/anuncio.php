2d523817879c1a42cedca5e0df361bd5335ab7f1
GaRbE666 <jaimegon17@gmail.com>
2023-08-17 13:52:00 +0200

    arreglando  el footer con php

--- ProyectoBienesRaices/anuncio.php
+++ ProyectoBienesRaices/anuncio.php
@@ -33,19 +33,6 @@
         </div>
     </main>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contactos.html">Contactos</a>
-            </nav>
-        </div>
-        <p class="copyright">Todos los derechos reservados 2023 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-
-</body>
-</html>
+<?php
+    include './includes/templates/footer.php';
+?> 
