30cbc6f5bf428dac00297834b027c96b3cb97b02
GaRbE666 <jaimegon17@gmail.com>
2023-08-17 22:59:37 +0200

    Creando el primer insert a bbdd

--- ProyectoBienesRaices/admin/propiedades/crear.php
+++ ProyectoBienesRaices/admin/propiedades/crear.php
@@ -4,9 +4,28 @@
     require '../../includes/config/database.php';
     $db = conectaDB();
 
-    echo "<pre>";
-    var_dump($_POST);
-    echo "</pre>";
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $garaje = $_POST['garaje'];
+        $vendedorId = $_POST['vendedor'];
+
+        //Insertar en la base de datos
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, garaje, vendedorId ) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$garaje', '$vendedorId')";
+
+        $resultado = mysqli_query($db, $query);
+
+        if($resultado){
+            echo "Insertado correctamente";
+        }
+    }
 
     require '../../includes/funciones.php';
     incluirTemplate('header');
@@ -27,30 +46,30 @@
                 <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
 
                 <label for="iamgen">Imagen:</label>
-                <input type="file" id="iamgen" name="" accept="image/jpeg, image/png">
+                <input type="file" id="iamgen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripci&oacute;n</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Informaci&oacute;n de la Propiedad</legend>
 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
 
                 <label for="wc">WC:</label>
-                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
 
                 <label for="garaje">Garaje:</label>
-                <input type="number" id="garaje" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="garaje" name="garaje" placeholder="Ej: 3" min="1" max="9">
 
             </fieldset>
 
             <fieldset>
                 <legend>Vendedor</legend>
 
-                <select>
+                <select name="vendedor">
                     <option value="1">Jaime</option>
                     <option value="2">Stephannie</option>
                 </select>
