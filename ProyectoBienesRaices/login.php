61b27f4d75abd50eac95a3f8ab8c8e2b14e9d1ed
GaRbE666 <jaimegon17@gmail.com>
2023-08-21 12:29:28 +0200

    Creando formulario para autenticar

---
+++ ProyectoBienesRaices/login.php
@@ -0,0 +1,28 @@
+<?php
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Iniciar Sesi&oacute;n</h1>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Email y Password</legend>
+
+                <label for="email">E-mail</label>
+                <input type="email" placeholder="Tu Email" id="email">
+
+                <label for="passwrod">Password</label>
+                <input type="password" placeholder="Tu Password" id="password">
+
+            </fieldset>
+
+            <input type="submit" value="Iniciar sersi&oacute;n" class="boton boton-verde">
+            
+        </form>
+    </main>
+
+<?php
+    incluirTemplate('footer');
+?> 
