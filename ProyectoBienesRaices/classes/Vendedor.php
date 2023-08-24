9d4787e2f1df19661948d64f44d880e5a459eefa
GaRbE666 <jaimegon17@gmail.com>
2023-08-24 20:52:52 +0200

    Modificando la clase Propiedad y Vendedores

--- ProyectoBienesRaices/classes/Vendedor.php
+++ ProyectoBienesRaices/classes/Vendedor.php
@@ -5,6 +5,22 @@ namespace App;
 class Vendedor extends ActiveRecord{
 
     protected static $tabla = 'vendedores';
+    protected static $columnasDb = ['id', 'nombre', 'apellido', 'telefono'];
+
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $telefono;
+
+    public function __construct($args = [])
+    {   
+
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->telefono = $args['telefono'] ?? '';
+
+    }
 
 }
 
