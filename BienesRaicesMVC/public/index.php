07d0a5127c66a6a667964624b67fab236a1f3975
GaRbE666 <jaimegon17@gmail.com>
2023-08-31 19:49:54 +0200

    Asociando url a metodos del controlador

--- BienesRaicesMVC/public/index.php
+++ BienesRaicesMVC/public/index.php
@@ -1,10 +1,15 @@
 <?php
    
-   use MVC\Router;
-
    require_once __DIR__ . '/../includes/app.php'; 
 
+   use MVC\Router;
+   use Controllers\PropiedadController;
+
     $router = new Router();
+
+    $router->get('/admin', [PropiedadController::class, 'index']);
+    $router->get('/propiedades/crear', [PropiedadController::class, 'create']);
+    $router->get('/propiedades/actualizar', [PropiedadController::class, 'update']);
 
     $router->comrprobarRutas();
 
