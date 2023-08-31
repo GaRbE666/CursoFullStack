e272ca8e019755cb95a2b87d255b0b9d7100900c
GaRbE666 <jaimegon17@gmail.com>
2023-08-31 20:00:01 +0200

    Creando el método para crear las vistas

--- BienesRaicesMVC/Router.php
+++ BienesRaicesMVC/Router.php
@@ -2,32 +2,37 @@
 
 namespace MVC;
 
-    class Router{
+class Router{
 
-        public $rutasGET = [];
-        public $rutarPOST = [];
+    public $rutasGET = [];
+    public $rutarPOST = [];
 
-        public function get($url, $fn){
-            $this->rutasGET[$url] = $fn;
-        }
-
-        public function comrprobarRutas()
-        {
-            $urlActual = $_SERVER['PATH_INFO'] ?? '/';
-            $metodo = $_SERVER['REQUEST_METHOD'];
-
-            if($metodo === 'GET'){
-                $fn = $this->rutasGET[$urlActual] ?? null;
-            }
-
-            if($fn){
-                //La URL existe y tiene una funcion asociada
-                call_user_func($fn, $this);
-            }else{
-                echo "Página no encontrada";
-            }
-        }
-
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
     }
+
+    public function comrprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if($fn){
+            //La URL existe y tiene una funcion asociada
+            call_user_func($fn, $this);
+        }else{
+            echo "Página no encontrada";
+        }
+    }
+
+    //Muestra una vista
+    public function render($view){
+        include __DIR__ . "/views/$view.php";
+    } 
+
+}
 
 ?>
