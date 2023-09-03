d9be060ef758833858a9c8ef16d741b3f9fce5e4
GaRbE666 <jaimegon17@gmail.com>
2023-09-03 18:00:20 +0200

    Configurando Mailtrap y enviando un email

--- BienesRaicesMVC/controllers/PaginasController.php
+++ BienesRaicesMVC/controllers/PaginasController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PaginasController{
 
@@ -56,6 +57,40 @@ class PaginasController{
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+            //Crear una instancia de PHPMailer
+            $mail = new PHPMailer();
+
+            //Configurar SMTP
+            $mail->isSMTP();
+            $mail->Host = 'sandbox.smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Username = '400767590fae95';
+            $mail->Password = 'b9c6fdcddb4618';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 2525;
+
+            //Configurar el contenido del email
+            $mail->setFrom('admin@bienesraices.com', 'BienesRaices.com');
+            $mail->addAddress('admin@bienesraices.com');
+            $mail->Subject = 'Tienes un Nuevo Mensaje';
+            
+            //Habilitar HTML
+            $mail->isHTML(true);
+            $mail->CharSet = 'UTF-8';
+
+            //Definir el contenido
+            $contenido = '<html> <p>Tienes un nuevo Mensaje</p> </html>';
+
+            $mail->Body = $contenido;
+            $mail->AltBody = 'Esto es texto alternatibo sin HTML';
+            
+            //Enviar el mail
+            if($mail->send()){
+                echo "Mensaje enviado Correctamente";
+            }else{
+                echo "El mensaje no se pudo enviar...";
+            }
+
         }
 
         $router->render('paginas/contacto', []);
@@ -64,3 +99,11 @@ class PaginasController{
 }
 
 ?>
+
+
+MAIL_MAILER=smtp
+MAIL_HOST=sandbox.smtp.mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=400767590fae95
+MAIL_PASSWORD=b9c6fdcddb4618
+MAIL_ENCRYPTION=tls
