2feadcef69193512c7d03d0b99736b15e6213944
GaRbE666 <jaimegon17@gmail.com>
2023-09-03 19:29:59 +0200

    Ocular o mostrar los campos del formulario segun la seleccion del usuario s

--- BienesRaicesMVC/src/js/app.js
+++ BienesRaicesMVC/src/js/app.js
@@ -37,7 +37,7 @@ function eventListeners(){
 
     //muestra campos condicionales
     const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
-    metodoContacto.addEventListener('click', mostrarMetodosContacto);
+    metodoContacto.forEach(input => input.addEventListener('click', mostrarMetodosContacto));
 
 }
 
@@ -47,6 +47,26 @@ function navegacionResponsive(){
     navegacion.classList.toggle('mostrar');
 }
 
-function mostrarMetodosContacto(){
+function mostrarMetodosContacto(e){
+    const contactoDiv = document.querySelector('#contacto');
 
+    if(e.target.value === 'telefono'){
+        contactoDiv.innerHTML = `
+            <label for="telefono">Tel&eacute;fono</label>
+            <input type="tel" placeholder="Tu Tel&eacute;fono" id="telefono" name="contacto[telefono]">
+
+            <p>Elija la fecha y la hora para la llamada</p>
+
+            <label for="fecha">Fecha</label>
+            <input type="date" id="fecha" name="contacto[fecha]">
+
+            <label for="hora">Hora</label>
+            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+        `;
+    }else{
+        contactoDiv.innerHTML = `
+            <label for="email">E-mail</label>
+            <input type="email" placeholder="Tu Email" id="email" name="contacto[email]" required>
+        `;
+    }
 }
--- BienesRaicesMVC/views/paginas/contacto.php
+++ BienesRaicesMVC/views/paginas/contacto.php
@@ -16,12 +16,6 @@
             <label for="nombre">Nombre</label>
             <input type="text" placeholder="Tu Nombre" id="nombre" name="contacto[nombre]" required>
 
-            <label for="email">E-mail</label>
-            <input type="email" placeholder="Tu Email" id="email" name="contacto[email]" required>
-
-            <label for="telefono">Tel&eacute;fono</label>
-            <input type="tel" placeholder="Tu Tel&eacute;fono" id="telefono" name="contacto[telefono]">
-
             <label for="mensaje">Mensaje</label>
             <textarea id="mensaje" name="contacto[mensaje]" required></textarea>
         </fieldset>
@@ -50,16 +44,11 @@
                 <input type="radio" value="telefono" id="contactar-telefono" name="contacto[contacto]" required>
 
                 <label for="contactar-email">E-mail</label>
-                <input type="radio" value="telefono" id="contactar-email" name="contacto[contacto]" required>
+                <input type="radio" value="email" id="contactar-email" name="contacto[contacto]" required>
             </div>
 
-            <p>Si eligi&oacute; tel&eacute;fono, elija la fecha y la hora</p>
+            <div id="contacto"></div>
 
-            <label for="fecha">Fecha</label>
-            <input type="date" id="fecha" name="contacto[fecha]">
-
-            <label for="hora">Hora</label>
-            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
 
         </fieldset>
 
