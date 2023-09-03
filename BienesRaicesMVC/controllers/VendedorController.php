dab24c8e8896cf76809b679cb7c1fd61432b29b4
GaRbE666 <jaimegon17@gmail.com>
2023-09-03 16:40:39 +0200

    Validacion de vendedores

--- BienesRaicesMVC/controllers/VendedorController.php
+++ BienesRaicesMVC/controllers/VendedorController.php
@@ -11,6 +11,23 @@ class VendedorController{
 
         $errores = Vendedor::getErrores();
         $vendedor = new Vendedor;
+
+        //Ejecutar el codigo despues de que el usuario envia el formulario
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            //Crear una nueva instancia
+            $vendedor = new Vendedor($_POST['vendedor']);
+
+            //Validar que no haya campos vacios
+            $errores = $vendedor->validar();
+
+            //No hay errores
+            if(empty($errores)){
+                $vendedor->guardar();
+            }
+
+        }
+
         $router->render('vendedores/crear',['errores' => $errores,
                                             'vendedor' => $vendedor]);
 
